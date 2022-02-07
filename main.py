8b4a9aecc1fd667cc50662a48f2c6ef6212f0e03
Alan888999 <arman.kmailbox@gmail.com>
2022-02-07 14:16:02 +0600

    task2

---
+++ main.py
@@ -0,0 +1,5 @@
+a = int(input("enter first number: "))
+b = int(input("enter second number: "))
+c = int(input("enter third number: "))
+d = int(input("enter forth number: "))
+print(((a+b)+(c+d))/2)
