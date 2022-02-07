cde07aea54ce9ea7475dcef060a1bb205cee5081
Alan888999 <arman.kmailbox@gmail.com>
2022-02-07 15:03:06 +0600

    task2

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 a = int(input("enter first number: "))
 b = int(a + 1)
 c = int(a - 1)
-print("After", a, "comes number ", b, "before", a, "comes number ", c)
+print("After", a, "comes number", b, "before", a, "comes number ", c)
